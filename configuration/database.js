2b0bbdbb5c60934ced05f4fe5406fa21249dc243
jagmohan123 <jagmohanrai082@gmail.com>
2024-05-16 15:39:37 +0530

    Database connectivity code

---
+++ configuration/database.js
@@ -0,0 +1,24 @@
+// import mongoose from "mongoose"
+/*import dotenv from "dotenv";
+ dotenv.config({
+     path:".env"
+ })*/
+const mongoose = require("mongoose");
+require("dotenv").config();
+function getDbConnect() {
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      // useNewUrlParser: true,
+      //   useUnifiedTopology:true,
+    })
+    .then(() => {
+      console.log("Database Connection Done");
+    })
+    .catch((e) => {
+      console.log(e);
+      console.log(e.message);
+      console.error("following error occured", e);
+      process.exit(1);
+    });
+}
+module.exports = getDbConnect;
